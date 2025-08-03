644e7ea7f6d8547fdfb36bd4e417e84fd57cd393
shivam24121 <Shivam050905@gmail.com>
2025-08-03 12:33:19 +0530

    Create Main.java

---
+++ 2074/A - Draw a Square/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner obj=new Scanner(System.in);
+        int t=obj.nextInt();
+        while(t-->0){
+            int a=obj.nextInt();
+            int b=obj.nextInt();
+            int c=obj.nextInt();
+            int d=obj.nextInt();
+            System.out.println((a==b && b==c && a==d)?"YES":"NO");
+        }
+    }
+}
