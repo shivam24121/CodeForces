85183468b9551a4adc1d6664e12957931344f39e
shivam24121 <Shivam050905@gmail.com>
2025-08-03 13:56:06 +0530

    Create Main.java

---
+++ 2071/A - The Play Never Ends/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner obj=new Scanner(System.in);
+        int t=obj.nextInt();
+        while(t-->0){
+            int n=obj.nextInt();
+            if(n%3==1){
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+        }
+    }
+}
