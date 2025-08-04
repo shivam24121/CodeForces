be433242d4e6227ad1985c7ab15061a5ea91a535
shivam24121 <Shivam050905@gmail.com>
2025-08-04 19:52:17 +0530

    Create Main.java

---
+++ 2108/A - Permutation Warm-Up/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner obj=new Scanner(System.in);
+        int t=obj.nextInt();
+        while(t-->0){
+            int n=obj.nextInt();
+            System.out.println((n*n/4)+1);
+        }
+    }
+}
