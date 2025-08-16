56b715ee7c659aee2a3b5a8ba671cf465df6c187
shivam24121 <Shivam050905@gmail.com>
2025-08-16 10:18:16 +0530

    Create Main.java

---
+++ 630/A - Again Twenty Five!/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner obj=new Scanner(System.in);
+        long n=obj.nextLong();
+        System.out.println(25);
+    }
+}
