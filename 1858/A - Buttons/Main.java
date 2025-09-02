37120532c203d974c9b99dc415578bddf271b2b3
shivam24121 <Shivam050905@gmail.com>
2025-09-02 12:05:04 +0530

    Create Main.java

---
+++ 1858/A - Buttons/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+        while(t-->0){
+            StringTokenizer st=new StringTokenizer(br.readLine());
+            int a=Integer.parseInt(st.nextToken());
+            int b=Integer.parseInt(st.nextToken());
+            int c=Integer.parseInt(st.nextToken());
+            
+            int lastC=c%2;
+            System.out.println(a+lastC>b?"First":"Second");
+        }
+    }
+}
