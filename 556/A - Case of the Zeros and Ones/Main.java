f99558aa4990cb2e323d626f1541167b76b122b5
shivam24121 <Shivam050905@gmail.com>
2025-09-30 22:09:58 +0530

    Update Main.java

--- 556/A - Case of the Zeros and Ones/Main.java
+++ 556/A - Case of the Zeros and Ones/Main.java
@@ -1,1 +1,20 @@
-
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        String s=br.readLine();
+        Stack<Character>st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(!st.isEmpty() && c!=st.peek()){
+                st.pop();
+            }
+            else{
+                st.push(c);
+            }
+        }
+        System.out.println(st.size());
+    }
+}
