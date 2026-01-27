fffd2b712c15ea71dd2a9f6cdf7d3b0c6d1e3884
shivam24121 <Shivam050905@gmail.com>
2026-01-27 17:02:58 +0530

    Create Main.java

---
+++ 1665/A - GCD vs LCM/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+        while(t-->0){
+            int n=Integer.parseInt(br.readLine());
+            System.out.println(n-3+" 1 "+"1 "+"1");
+        }
+    }
+}
