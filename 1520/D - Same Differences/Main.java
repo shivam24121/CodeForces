4e1cd6c5d05dfcc6a6c5628a60a4a9ad19cdaa97
shivam24121 <Shivam050905@gmail.com>
2026-02-14 21:42:23 +0530

    Create Main.java

---
+++ 1520/D - Same Differences/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner obj=new Scanner(System.in);
+        int t=obj.nextInt();
+        while(t-->0){
+            int n=obj.nextInt();
+            int[] a=new int[n];
+            for(int i=0;i<n;i++){
+                a[i]=obj.nextInt();
+                a[i]-=i;
+            }
+            Map<Integer,Integer>map=new HashMap<>();
+            for(int i:a){
+                map.put(i,map.getOrDefault(i,0)+1);
+            }
+            long res=0;
+            for(var e:map.values()){
+                res+=(long)e*(e-1)/2;
+            }
+            System.out.println(res);
+        }
+    }
+}
