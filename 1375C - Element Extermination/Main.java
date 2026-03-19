c001d6459fbeb75e4721e9a930e7d3c593b414ef
shivam24121 <Shivam050905@gmail.com>
2026-03-19 15:19:54 +0530

    Create Main.java

---
+++ 1375C - Element Extermination/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner obj=new Scanner(System.in);
+        int t=obj.nextInt();
+        while(t-->0){
+            int n=obj.nextInt();
+            int[] a=new int[n];
+            for(int i=0;i<n;i++){
+                a[i]=obj.nextInt();
+            }
+            System.out.println(a[n-1]>a[0]?"YES":"NO");
+        }
+    }
+}
