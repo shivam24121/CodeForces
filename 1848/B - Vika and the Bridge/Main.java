a5808730d25327f5c0beea99c4294bccf4c6af74
shivam24121 <Shivam050905@gmail.com>
2026-04-16 21:33:35 +0530

    Create Main.java

---
+++ 1848/B - Vika and the Bridge/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws java.lang.Exception{
+        
+        BufferedReader br;
+        br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int t=Integer.parseInt(br.readLine());
+        while(t-->0){
+            
+            int n=Integer.parseInt(br.readLine());
+            
+            for(int i=0;i<n;i++){
+                System.out.print(2*i+1+" ");
+            }
+            System.out.println();
+            
+            // System.out.println(ans);
+        }
+    }
+}
