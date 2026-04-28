a9f02f06d034ba004f6e7013ace67e82727a0801
shivam24121 <Shivam050905@gmail.com>
2026-04-28 21:53:01 +0530

    Create Main.java

---
+++ 1096/A - Find Divisible/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner obj=new Scanner(System.in);
+        int t=obj.nextInt();
+        while(t-->0){
+            int l=obj.nextInt();
+            int r=obj.nextInt();
+            System.out.println(l+" "+2*l);
+        }
+    }
+}
