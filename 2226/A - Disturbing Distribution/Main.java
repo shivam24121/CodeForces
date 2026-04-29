1af496d049642104aa2e3a2db9ce860680376e49
shivam24121 <Shivam050905@gmail.com>
2026-04-29 22:22:05 +0530

    Create Main.java

---
+++ 2226/A - Disturbing Distribution/Main.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws java.lang.Exception{
+        
+        BufferedReader br;
+        br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int t=Integer.parseInt(br.readLine());
+        while(t-->0){
+            
+            int n=Integer.parseInt(br.readLine());
+            st=new StringTokenizer(br.readLine());
+            
+            int[] arr=new int[n];
+            
+            for(int i=0;i<n;i++){
+                arr[i]=Integer.parseInt(st.nextToken());
+            }
+            
+            int res=arr[n-1]==1?1:0;
+            
+            for(int i:arr){
+                if(i!=1){
+                    res+=i;
+                }
+            }
+            
+            System.out.println(res);
+        }
+    }
+}
