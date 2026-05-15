241ea35b1ab3926d49f8a9d3da82f2336b72b9bf
shivam24121 <Shivam050905@gmail.com>
2026-05-15 21:39:20 +0530

    Create Main.java

---
+++ 1932/B - Chaya Calendar/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws java.lang.Exception{
+        
+        BufferedReader br;
+        br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int t=Integer.parseInt(br.readLine());
+        while(t-->0){
+            
+            int n=Integer.parseInt(br.readLine());
+            st=new StringTokenizer(br.readLine());
+            
+            long ans=0;
+            
+            for(int i=0;i<n;i++){
+                long x=Long.parseLong(st.nextToken());
+                ans=(ans/x+1)*x;
+            }
+            
+            System.out.println(ans);
+        }
+    }
+}
